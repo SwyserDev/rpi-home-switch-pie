69394cb2ff2a5cee8e978eb422aee909d55b2cfc
Francois van der Merwe <francoisvandermerwe@standardbank.co.za>
2016-12-22 16:59:42 +0200

    setup basic gpio on off api

--- pieRoutes.js
+++ pieRoutes.js
@@ -4,7 +4,7 @@
     var express = require('express');
     var pieRoutes = express.Router();
     var Gpio = require('onoff').Gpio;
-    // var led = new Gpio(14, 'out');
+    var led = new Gpio(14, 'out');
 
     pieRoutes.get('/gpio14on', function(req, res) {
       res.status(200).json({ status: 'on' });
